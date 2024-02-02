504e7c23a399227212b4b338e204f999a9766d76
hmoa01 <hmoa01@gmail.com>
2024-02-02 15:34:17 +0100

    add first geometry with gsap animation

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,8 +4,9 @@ import { Content, KeyTextField } from "@prismicio/client";
 import { useEffect, useRef } from "react";
 
 import Bounded from "@/components/Bounded";
+import Shapes from "./Shapes";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 
 /**
  * Props for `Hero`.
@@ -77,6 +78,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1
             className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
